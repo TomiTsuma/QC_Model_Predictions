520d704647dd078f0f9a0222c2ce87b59c24afb3
TomiTsuma <tommytsuma7@gmail.com>
2025-07-18 09:24:34 +0000

    latest

--- predict.py
+++ predict.py
@@ -49,28 +49,6 @@ def predict_chems(path_to_model, predction_folder_path, chemicals, model_version
         print(f'Starting prediction using model version {model_version}')
         base_path = Path(path_to_model)
         for chemical in chemicals:
-            # chems = [
-            #     'aluminium',
-            #     'phosphorus',
-            #     'ph',
-            #     'exchangeable_acidity',
-            #     'calcium',
-            #     'magnesium',
-            #     'sulphur',
-            #     'sodium',
-            #     'iron',
-            #     'manganese',
-            #     'boron',
-            #     'copper',
-            #     'zinc',
-            #     'total_nitrogen',
-            #     'potassium',
-            #     'ec_salts',
-            #     'organic_carbon', 'cec',
-            #     'sand', 'silt', 'clay'
-            # ]
-            # if chemical not in chems:
-            #     continue
             print(chemical)
             preds_comb = pd.DataFrame()
             models_folder = base_path / chemical / 'std'
@@ -78,12 +56,12 @@ def predict_chems(path_to_model, predction_folder_path, chemicals, model_version
             all_models = [x for x in models_folder.glob('**/*.hdf5')]
 
             data = data
-            if os.getenv("SENSOR_ID") == "2" and os.getenv("SAMPLE_TYPE_ID") == "2" and os.getenv("SAMPLE_PRETREATMENT_ID") == "3" and chemical not in ['total_nitrogen','total_carbon']:
-                data = data.apply(lambda row: (row - np.mean(row)) / np.std(row, ddof=1), axis=1)
+            # if os.getenv("SENSOR_ID") == "2" and os.getenv("SAMPLE_TYPE_ID") == "2" and os.getenv("SAMPLE_PRETREATMENT_ID") == "3" and chemical not in ['total_nitrogen','total_carbon']:
+            #     data = data.apply(lambda row: (row - np.mean(row)) / np.std(row, ddof=1), axis=1)
             new_indices = data.index
 
             for model_path in all_models:
-                print(model_path)
+
                 json_path = model_path.parent.parent / 'model.json'
 
                 with open(json_path) as f:
@@ -93,15 +71,32 @@ def predict_chems(path_to_model, predction_folder_path, chemicals, model_version
 
                 for i in range(len(json_['Inputs'])):
                     input_name = json_['Inputs'][i]['Name']
-#                     print(f'filename: {filename}')
                     train = data.copy(deep=True)
+                    is_epo_present = False
                     for j in range(len(json_['Inputs'][i]['Pre-processing'])):
+                        
                         key_ = json_['Inputs'][i]['Pre-processing'][j]['Name']
+                        
+                        if key_ == "EPO":
+                            is_epo_present = True
+                            train = train.sub(train.mean(axis=1), axis=0).div(train.std(axis=1), axis=0)
+                            epo_matrix = pd.read_csv(os.path.join(path_to_model, model_version, "epo_matrix.csv"),index_col=0)
+                            train = np.dot(train.values, epo_matrix.values)
+                            
+                            train = pd.DataFrame(train)
+                            continue
+
                         if input_name == 'nir2':
                             input_name = 'nir.2'
+                        if is_epo_present == False:
+                            position = j
+                            pickle_path = model_path.parent / 'preprocess' / \
+                                f'input.{input_name}.{position}.{key_}.pickle'
+                        else:
+                            position = j-1
 
-                        pickle_path = model_path.parent / 'preprocess' / \
-                            f'input.{input_name}.{j}.{key_}.pickle'
+                            pickle_path = model_path.parent / 'preprocess' / \
+                                f'input.{input_name}.{position}.{key_}.pickle'
                         pickle_ = joblib.load(pickle_path)
                         train = pickle_.fit_transform(train)
 
@@ -111,6 +106,7 @@ def predict_chems(path_to_model, predction_folder_path, chemicals, model_version
                 model = tf.keras.models.load_model(model_path, compile=False)
                 preds = pd.DataFrame(model(inputs).numpy())
                 preds_comb = pd.concat([preds_comb, preds], axis=1)
+
             preds_comb = preds_comb.median(axis=1)
 
             preds_comb.index = new_indices
@@ -122,3 +118,15 @@ def predict_chems(path_to_model, predction_folder_path, chemicals, model_version
         print(f'Finalizing prediction using model version {model_version}')
 
 
+predict(
+    chemicals=["ph","sodium"],
+    data=pd.read_csv(f"/home/tom/DSML125/MSSC_DVC/spc/spc.csv",index_col=0, engine="c"),
+    predction_folder_path=f"/home/tom/DSML125/outputFiles/predictions",
+    model_versions=["v1.0"],
+    path_to_model=f"/home/tom/DSML125/QC_Model_Predictions/soil-alpha-atr-epo-fritsch-2mm/v1.0",
+    output_path=f"/home/tom/DSML125/outputFiles/evaluation",
+    project_name= project_name, 
+    path_to_splits=f"/home/tom/DSML125/MSSC_DVC/{data_item}",
+
+    data_version = data_item
+)
